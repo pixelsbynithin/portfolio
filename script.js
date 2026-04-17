acac02c8ce522805d25cf6f4c01b341f5420e2d3
Nithin Mohan <pixelsbynithin@gmail.com>
2026-04-18 04:48:39 +0530

    Smooth scroll and spacer issue fix before zoom cont

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: t => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smoothWheel: true,
+  smoothTouch: false,
+});
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
+
+
+lenis.on('scroll', ScrollTrigger.update);
+
 gsap.registerPlugin(ScrollTrigger);
 
 
